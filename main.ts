5442aa545538d9d72b47159b237f1c2b3538ba46
rethamoldenupa84 <rethamoldenupa84@gmail.com>
2025-07-06 20:53:44 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,6 @@
 import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
 
-Deno.serve((req) =>
-  serveDir(req, {
-    fsRoot: "./",         // Make sure your index.html is in this same folder
-    showDirListing: false // Hides file listing for visitors
-  })
-);
+Deno.serve((req) => serveDir(req, {
+  fsRoot: "./",
+  showDirListing: false
+}));
